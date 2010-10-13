Homebrew/legacy-homebrew

Make R14B default and head Erlang version
 - Use R14B documentation

 - Use R14B tag for GitHub download

Library/Formula/erlang.rb
@@ -1,22 +1,22 @@
 require 'formula'
 
 class ErlangManuals <Formula
-  url 'http://www.erlang.org/download/otp_doc_man_R13B04.tar.gz'
-  md5 '681aaef70affc64743f4e8c0675034af'
+  url 'http://erlang.org/download/otp_doc_man_R14B.tar.gz'
+  md5 '011530a24fbcc194be9bd01f779325a2'
 end
 
 class ErlangHeadManuals <Formula
-  url 'http://www.erlang.org/download/otp_doc_man_R14A.tar.gz'
-  md5 'b57a7846818ad144b1b6ecc0a54de2ae'
+  url 'http://erlang.org/download/otp_doc_man_R14B.tar.gz'
+  md5 '011530a24fbcc194be9bd01f779325a2'
 end
 
 class Erlang <Formula
   # Download from GitHub. Much faster than official tarball.
-  url "git://github.com/erlang/otp.git", :tag => "OTP_R13B04"
-  version 'R13B04'
+  url "git://github.com/erlang/otp.git", :tag => "OTP_R14B"
+  version 'R14B'
   homepage 'http://www.erlang.org'
 
-  head "git://github.com/erlang/otp.git", :tag => "OTP_R14A"
+  head "git://github.com/erlang/otp.git", :tag => "OTP_R14B"
 
   # We can't strip the beam executables or any plugins, there isn't really
   # anything else worth stripping and it takes a really, long time to run

blakesmith/homebrew

Signed-off-by: Adam Vandenberg <flangy@gmail.com>
 - Move head up to dev branch

Library/Formula/erlang.rb
@@ -1,22 +1,22 @@
 require 'formula'
 
 class ErlangManuals <Formula
-  url 'http://www.erlang.org/download/otp_doc_man_R13B04.tar.gz'
-  md5 '681aaef70affc64743f4e8c0675034af'
+  url 'http://erlang.org/download/otp_doc_man_R14B.tar.gz'
+  md5 '011530a24fbcc194be9bd01f779325a2'
 end
 
 class ErlangHeadManuals <Formula
-  url 'http://www.erlang.org/download/otp_doc_man_R14A.tar.gz'
-  md5 'b57a7846818ad144b1b6ecc0a54de2ae'
+  url 'http://erlang.org/download/otp_doc_man_R14B.tar.gz'
+  md5 '011530a24fbcc194be9bd01f779325a2'
 end
 
 class Erlang <Formula
   # Download from GitHub. Much faster than official tarball.
-  url "git://github.com/erlang/otp.git", :tag => "OTP_R13B04"
-  version 'R13B04'
+  url "git://github.com/erlang/otp.git", :tag => "OTP_R14B"
+  version 'R14B'
   homepage 'http://www.erlang.org'
 
-  head "git://github.com/erlang/otp.git", :tag => "OTP_R14A"
+  head "git://github.com/erlang/otp.git", :branch => "dev"
 
   # We can't strip the beam executables or any plugins, there isn't really
   # anything else worth stripping and it takes a really, long time to run